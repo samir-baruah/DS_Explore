3df2f0cf68d525e53805e63d4e669259ef286ccc
SAMIR BARUAH <61110610+samir-baruah@users.noreply.github.com>
2020-03-22 19:25:58 +0530

    Add files via upload

---
+++ Capstone_Project_Notebook.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This is a notebook to be pushed to Githib, and this will be used for Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
