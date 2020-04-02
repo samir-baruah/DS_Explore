66aeed6b692034a53d978afb3d8623dccdc4a9f2
SAMIR BARUAH <61110610+samir-baruah@users.noreply.github.com>
2020-04-02 10:25:36 +0530

    Delete python_code.py

--- python_code.py
+++
@@ -1,9 +0,0 @@
-
-i print("Hello")
-bash: syntax error near unexpected token `"Hello"iiiiiiiiiiiiiiiiiiiiiiiiiiiii
-print ("Hello GitHub!")
-print("Hello World"
-        :wq
-cd ~/Desktop/projects/DS_EXplore
-print("Hello World")
-vim.python_code.py
